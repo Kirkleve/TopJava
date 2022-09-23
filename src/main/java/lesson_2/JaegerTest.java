a0ca79d550c56d66851c18dcdef76c63ef34ab34
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-09-23 23:16:57 +0200

    next GuessNumbe

---
+++ src/main/java/lesson_2/JaegerTest.java
@@ -0,0 +1,19 @@
+package lesson_2;
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger crimsonTyphoon = new Jaeger();
+        crimsonTyphoon.setModelName("Crimson Typhoon");
+        crimsonTyphoon.setMark("Mark-4");
+        crimsonTyphoon.setOrigin("China");
+        crimsonTyphoon.setHeight(76.2);
+        crimsonTyphoon.setWeight(1.722);
+        crimsonTyphoon.setSpeed(9);
+        crimsonTyphoon.setStrength(8);
+        crimsonTyphoon.setArmor(6);
+        Jaeger strikerEureka = new Jaeger("Striker Eureka", "Mark-5", "Australia",
+                76.2, 1.850, 10, 10, 9);
+        crimsonTyphoon.informationAboutJaeger(crimsonTyphoon);
+        strikerEureka.informationAboutJaeger(strikerEureka);
+    }
+}
